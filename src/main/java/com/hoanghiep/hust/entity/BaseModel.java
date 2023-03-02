56b6e5057c11f6558bf1b16877cfc4dd31605d08
hiep.hoang <hiep.hoang@savvycomsoftware.com>
2023-03-02 21:40:34 +0700

    fix id auto increment

--- src/main/java/com/hoanghiep/hust/entity/BaseModel.java
+++ src/main/java/com/hoanghiep/hust/entity/BaseModel.java
@@ -10,7 +10,7 @@ import javax.persistence.MappedSuperclass;
 public abstract class BaseModel {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 
